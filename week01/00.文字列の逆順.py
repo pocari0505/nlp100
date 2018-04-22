b57683f58f74e93a6ff4a8a5b1b1d8e9e7b1bfad
otsuka-pocari <akinori.pocari@gmail.com>
2018-04-22 09:30:22 +0900

    first commit

---
+++ week01/00.文字列の逆順.py
@@ -0,0 +1,3 @@
+s='stressed'
+print(s)
+print(s[::-1])
---
+++ week01/02.「パトカー」＋「タクシー」＝「パタトクカシーー」.py
@@ -0,0 +1,5 @@
+s1="パトカー"
+s2="タクシー"
+ln=len(s1) if len(s1)<len(s2) else len(s2)
+print(s1+' + '+ s2)
+print (''.join([s1[i]+s2[i] for i in range(ln)])+s1[ln:]+s2[ln:])
